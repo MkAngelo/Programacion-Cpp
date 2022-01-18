8eb5e45cddab883bbb201293a9d2adfb5b5982f6
MkAngelo <mikequi000@gmail.com>
2022-01-18 01:57:32 -0600

    Constants and Lists

--- variables.cc
+++ variables.cc
@@ -4,5 +4,12 @@ using namespace std;
 
 int main(){
     int edad = 18;
-    cout << edad;
+    const char letra = 'A'; // No cambia su valor
+    float R[10] = {2, 32, 4.6, 2, 1, 0.5, 3, 8, 0, 12}; // Lista definida
+    float S[] = {2, 32, 4.6, 2, 1, 0.5, 3, 8, 0, 12}; // Lista
+    int N[] = {1, 2, 3, 6};
+    char Mensaje[] = "Soy un Script shhhhh";
+    char Saludo[] = {'H', 'o', 'l', 'a', 0};
+    cout << Mensaje << endl;
+    cout << letra;
 }
