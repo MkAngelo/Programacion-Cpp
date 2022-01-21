7f7c15a978c5adc2eede69d8cd5055afbc9b6682
MkAngelo <mikequi000@gmail.com>
2022-01-21 01:05:28 -0600

    Destructor

--- clases.cc
+++ clases.cc
@@ -5,18 +5,24 @@ using namespace std;
 
 class Persona {
     public:
-        string nombre = "Miguel";
+        string nombre;
         int edad;
+        Persona(string n) {
+            nombre = n;
+        }
+        ~Persona(){
+            cout << "Destructor" << endl;
+        }        
         void saludar() {
             cout << nombre << endl;
         }
 };
 
 int main() {
-    Persona *p = new Persona();
-    Persona *p2 = new Persona();
-
-    p2->nombre = "Diana";
+    Persona *p = new Persona("Miguel");
+    Persona *p2 = new Persona("Diana");
+    delete p2; // Forma manual
+    //p2->nombre = "Diana";
     //cout << p->nombre << endl;
     p->saludar();
     p2->saludar();
