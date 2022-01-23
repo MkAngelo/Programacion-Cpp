78bd764c82a3dc73126bc9974f4eac87b2bcc18f
MkAngelo <mikequi000@gmail.com>
2022-01-22 23:42:10 -0600

    This part 1

--- clases.cc
+++ clases.cc
@@ -8,24 +8,35 @@ class Persona {
         string nombre;
         int edad;
     public:
-        Persona(string n) {
-            nombre = n;
+        Persona(string nombre, int edad) {
+            this->nombre = nombre;
+            this->edad = edad;
         }
         // Persona(string n, int e) : nombre(n), edad(e) {}
         ~Persona(){
             cout << "Destructor" << endl;
         }        
+        void establecerNombre(string nombre) {
+            this->nombre = nombre;
+        }
+        void establecerEdad(int edad) {
+            this->edad = edad;
+        }
         void saludar() {
-            cout << nombre << endl;
+            cout << "Hi my name is " << nombre << ", I'm  " << edad << " years old." << endl;
         }
 };
 
 int main() {
-    Persona *p = new Persona("Miguel");
-    Persona *p2 = new Persona("Diana");
+    Persona *p = new Persona("Miguel", 20);
+    //Persona *p2 = new Persona("Diana", 19);
+
+    p->establecerNombre("Angel"); 
+    p->establecerEdad(21);
+
     //delete p2; // Forma manual
     //p2->nombre = "Diana";
     //cout << p->nombre << endl;
     p->saludar();
-    p2->saludar();
+    //p2->saludar();
 }
